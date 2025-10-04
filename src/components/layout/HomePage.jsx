144b32d3ee0f4f9542fd0ab78302026f41367733
Soyab-Aktar <iamsoyabcode23@gmail.com>
2025-10-04 08:37:05 +0530

    Project Setup

---
+++ src/components/layout/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <nav>
+        <Navbar></Navbar>
+      </nav>
+    </div>
+  );
+};
+
+export default HomePage;
